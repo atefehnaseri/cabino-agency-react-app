dd0a9b36cadbaf9719700cf4391ca6e2a19dd9ec
Atefeh Naseri <ati_nas@yahoo.com>
2025-06-30 17:00:22 +0200

    chore(styles): add global styles and ui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,32 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/globalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
 `;
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-`;
 
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
-
-const StyledApp = styled.div`
+const StyledApp = styled.main`
   background-color: orange;
+  padding: 20px;
 `;
 
 function App() {
   return (
-    <StyledApp>
-      <H1>hello</H1>
-      <Button>Check In</Button>
-      <Button>Check Out</Button>
-      <Input placeholder="Number of guests" type="number" />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">hello</Heading>
+        <Heading as="h2">h2</Heading>
+        <Button>Check In</Button>
+        <Button>Check Out</Button>
+        <Input placeholder="Number of guests" type="number" />
+      </StyledApp>
+    </>
   );
 }
 
