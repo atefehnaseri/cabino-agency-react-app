d23ec2702b52812af0f52dd96ebe3920d706a54b
Atefeh Naseri <ati_nas@yahoo.com>
2025-07-01 12:24:41 +0200

    chore(appNav): add app navigation into sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,13 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
-            <Route element={<Dashboard />}>
-              <Route index element={<Navigate replace to="dashboard" />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="bookings" element={<Bookings />} />
-              <Route path="cabins" element={<Cabins />} />
-              <Route path="users" element={<Users />} />
-              <Route path="settings" element={<Settings />} />
-              <Route path="account" element={<Account />} />
-            </Route>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
           </Route>
 
           <Route path="login" element={<Login />} />
