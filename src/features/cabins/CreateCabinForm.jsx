c839d7f4e261484a694109bc6e79e0fff5ef0d58
Atefeh Naseri <ati_nas@yahoo.com>
2025-07-08 13:03:54 +0200

    feat(createCabinForm): add a modal for creating cabin  using portal

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -10,7 +10,7 @@ import FormRow from "../../ui/FormRow";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, handleCloseForm }) {
   const { isCreating, createCabin } = useCreateCabin();
   const { isEditing, editCabin } = useEditCabin();
   const isPendingForm = isCreating || isEditing;
@@ -35,6 +35,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
             //the newly created cabin
             console.log(data);
             reset();
+            handleCloseForm?.();
           },
         }
       );
@@ -45,7 +46,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      type={handleCloseForm ? "modal" : "regular"}
+    >
       <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input
           type="text"
@@ -131,7 +135,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button
+          variation="secondary"
+          type="reset"
+          onClick={() => handleCloseForm?.()}
+        >
           Cancel
         </Button>
         <Button disabled={isPendingForm}>
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,5 +1,5 @@
+import AddCabin from "../features/cabins/AddCabin";
 import CabinTable from "../features/cabins/CabinTable";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
@@ -12,7 +12,7 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
-        <CreateCabinForm />
+        <AddCabin />
       </Row>
     </>
   );
