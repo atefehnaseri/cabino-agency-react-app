c6e500b7a30fc231f553167364bb1eea3de748a2
Atefeh Naseri <ati_nas@yahoo.com>
2025-07-11 16:06:39 +0200

    feat(bookingTable): add sort and logics to bookings page

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -12,13 +12,18 @@ export function useBookings() {
       ? null
       : { field: "status", value: filterValue, method: "eq" }; //method can be any methods of supabase like "gt", "gte" (grather than equal an etc.)
 
+  //Sort
+  const sortByParam = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByParam.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     error,
     data: bookings,
   } = useQuery({
-    queryKey: ["bookings", filter], //like dependancy array if the filter object changed the fetch will be run again
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy], //like dependancy array if the filter object changed the fetch will be run again
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
