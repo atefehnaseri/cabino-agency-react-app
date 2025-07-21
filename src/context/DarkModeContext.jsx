33e9fdfbfd5fd080a7ff4086e417aac69a8184ba
Atefeh Naseri <ati_nas@yahoo.com>
2025-07-21 15:40:51 +0200

    chore(netlifyConfig): add netlify configuration file into public

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,6 +1,5 @@
 import { createContext, useContext, useEffect } from "react";
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
-import { is } from "date-fns/locale";
 
 const DarkModeContext = createContext();
 
